e390187a1b96807627c6e6c891b9ebd94274f572
Pritam <manoharipritam@gmail.com>
2024-05-05 13:11:51 +0530

    add matchDetails API

--- src/components/MatchDetails.jsx
+++ src/components/MatchDetails.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const MatchDetails = () => {
-    let {id} = useParams();
+    let { id } = useParams();
+
+    const getMatchInfo = async () => {
+        const url = 'https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/89654';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+                'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getMatchInfo();
+    }, []);
 
     return (
         <>
